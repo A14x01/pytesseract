3657c93935311d7eb8a342a8d036f13e8de73eb5
Alexandr <117397307+A14x01@users.noreply.github.com>
2022-11-23 17:08:16 +0100

    Update from PIL import image.py

--- from PIL import image.py
+++ from PIL import image.py
@@ -4,23 +4,30 @@ import pytesseract
 print('This python code can read text from files and convert it to just a text')
 
 while True:
-    try:
+    print('If you want to contineu type (1).\nIf you want to exit type (2)')
+    user_answer = input('')
+    if user_answer == '1':
+        try:
+            print('Type the path to the tesseract.exe file: ')
+            print("Example: E:/x/x/x.exe")
 
-        print('Type the path to the file: ')
-        print("Example: E:/x/x/x.exe")
+            user_path = input(r'')
 
-        user_path = input(r'')
+            pytesseract.pytesseract.tesseract_cmd = user_path
 
-        pytesseract.pytesseract.tesseract_cmd = user_path
+            print('Please put your picture in the same folder as your python code')
+            user_image = input('Type name of the .png file:\n')
 
-        print('Please put your picture in the same folder as your python code')
-        user_image = input('Type name of the .png file:\n')
+            img = Image.open(user_image)
+            text = pytesseract.image_to_string(img)
 
-        img = Image.open(user_image)
-        text = pytesseract.image_to_string(img)
+            print('Please wait\n')
 
-        print(text)
-        break
-    except:
-        print("Wrong path to file or name of the .png file")
-        continue
+            print(text)
+        except:
+            print("Wrong path to file or name of the .png file")
+            continue
+    elif user_answer == '2':
+        quit()
+    else:
+        print("Wrong number")
